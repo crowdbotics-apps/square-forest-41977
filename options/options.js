c11304c04bf5a8b0c0953c805cfa0a260b5508ad
Crowdbotics <team@crowdbotics.com>
2023-07-07 15:19:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "square-forest-41977",
+  url: "https://square-forest-41977.botics.co",
+  api: "https://square-forest-41977.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
